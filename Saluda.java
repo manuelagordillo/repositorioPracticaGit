77f60a28bb24fd5731f7c0bd014612f8e358e3e6
manuelagordillo <81710091+manuelagordillo@users.noreply.github.com>
2022-09-28 19:53:59 +0200

    FIcheros proyecto git

---
+++ Saluda.java
@@ -0,0 +1,30 @@
+package paqueteGitHub;
+
+public class Saluda{
+
+	public static void main(String[] args){
+		System.out.println("Hola chavales, ¿cómo estáis?");
+		System.out.println("Estoy mu bien.Soy Juan Antonio López Mera");
+		System.out.println("Estoy mu bien.Soy Antonio Barrera Romero");
+		System.out.println("Yo no suplico por dieces, suplico por cincos");
+		System.out.println("Última práctica!!! Belén Santos Alonso.");
+		System.out.println("Hola, soy Sergio Gavela. Algo justo de tiempo para entregar el trabajo pero bien")
+		System.out.println("Hola, me llamo Raúl Casado Moreno");
+    		System.out.println("Yo estoy increible.Soy Ismael Ruiz Carlin");
+		System.out.println("Hola, ¿que tal?. Jesus Ramirez Richarte");
+		System.out.println("Hola, estoy muy bien. Soy Ariana Martín Martínez");
+		System.out.println("Aquí Raúl Aguilar García, necesito unas vacaciones :D");
+		System.out.println("Buenas, soy Alejandro Salazar. Aqui programando en la playa.");
+		System.out.println("Buenas, soy Daniel Mulero Troya. Estoy bien pero mejor en la playa");
+    
+    		System.out.println("Holita, Juan. Soy Tania Rodriguez. Taky para los amigos ;)");
+
+    		System.out.println("Con ganas de vacaciones ya!!.Soy Lorena González.");
+    		System.out.println("No sé que hago aqui, espero que esté bien. Lorena Jiménez/loresodio <]:{v");
+		System.out.println("Por aquí Karina AKA Elli. Por fin llega verano :)");
+        
+   		System.out.println("Hola, soy Nuria Sánchez Manzanares");
+   		System.out.println("Hola, soy Claudia Berrocal, Felices fiestas!");
+		System.out.println("Hola, soy Manuel Palomo Pró, ¡Buen verano!");
+	}
+}
